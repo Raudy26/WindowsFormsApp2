1d8fa8a910bb4a724eb2cc4c7a80654cb339efa7
PC <PC@DESKTOP-8HGPVBP>
2024-09-18 12:36:29 -0400

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MultiplicacionSinAsterisco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable necesaria para el diseñador.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // Controles del formulario
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label lblResultado;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+      
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // txtNumero1
+            this.txtNumero1.Location = new System.Drawing.Point(50, 30);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 0;
+
+            // txtNumero2
+            this.txtNumero2.Location = new System.Drawing.Point(50, 70);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 1;
+
+            // btnMultiplicar
+            this.btnMultiplicar.Location = new System.Drawing.Point(50, 110);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(100, 23);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+
+            // lblResultado
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(50, 150);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(58, 13);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "Resultado:";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Multiplicación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MultiplicacionSinAsterisco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            
+            int num1 = int.Parse(txtNumero1.Text);
+            int num2 = int.Parse(txtNumero2.Text);
+
+          
+            int resultado = MultiplicarSinAsterisco(num1, num2);
+
+            
+            lblResultado.Text = "Resultado: " + resultado.ToString();
+        }
+
+        
+        private int MultiplicarSinAsterisco(int a, int b)
+        {
+            int resultado = 0;
+
+            
+            bool negativo = false;
+            if (b < 0)
+            {
+                b = -b;
+                negativo = true;
+            }
+
+           
+            for (int i = 0; i < b; i++)
+            {
+                resultado += a;
+            }
+
+           
+            if (negativo)
+            {
+                resultado = -resultado;
+            }
+
+            return resultado;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
